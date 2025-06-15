0deb86cf8dc1a07654520dbbb6fc2c982e4f924e
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-06-15 06:26:07 -0300

    Added Test Project as well as started working on ai agent functions, left off on working with the get files info function

---
+++ functions/get_files_info.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+def get_files_info(working_directory, directory=None):
+    if os.path.isdir(working_directory):
+        
+        if directory and os.path.isdir(directory):
+            pass
+
--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import os
 from dotenv import load_dotenv
 import sys
+from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-from google import genai
+
+
 
 client = genai.Client(api_key=api_key)
-
 prompt = sys.argv
+
 if len(prompt) <= 1:
     sys.exit(1)
 else:
-
-    prompt_list = prompt[1:]
+    verbose_status= False
+    prompt_list = prompt[1:-1]
+    if prompt[-1] == "--verbose":
+        verbose_status = True
     prompt_string= " ".join(prompt_list)
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt_list)
+    messages=[types.Content(role="user", parts=[types.Part(text=prompt_string)])]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-  
+    if verbose_status:
+        print(f"User prompt: {prompt_string}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
