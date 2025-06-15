3f64f4ccba492ef427f8a13e38e46730c0f7f705
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-06-15 03:02:30 -0300

    added support for user commands

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ from google import genai
 client = genai.Client(api_key=api_key)
 
 prompt = sys.argv
-prompt_list = prompt[1:]
-prompt_string= " ".join(prompt_list)
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt_list)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if len(prompt) <= 1:
+    sys.exit(1)
+else:
+
+    prompt_list = prompt[1:]
+    prompt_string= " ".join(prompt_list)
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt_list)
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+  
