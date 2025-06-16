f3e551e1c1e7ea051209ad92f46fe5e94c2027ed
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-06-16 06:22:28 -0300

    its a long nighjt, can't thinik or type, im headin to bed, did some stuff, no documnentatioin fuck you figure it out

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,9 +1,20 @@
 import os
 import sys
-
+from pprint import pprint
 def get_files_info(working_directory, directory=None):
     if os.path.isdir(working_directory):
+        files_path = os.path.join(working_directory,directory)
         
-        if directory and os.path.isdir(directory):
-            pass
+        print(files_path)
+        if os.path.isdir(files_path):
+            lst_dir_content = os.listdir(files_path)
+            srt_rep_dict = {
 
+            }
+            for content in lst_dir_content:
+                srt_rep_dict[content] = f"file_size={os.path.getsize(os.path.join(os.path.abspath(working_directory),directory, content))} bytes, is_dir={os.path.isdir(os.path.join(os.path.abspath(working_directory),directory, content))}"
+            return srt_rep_dict
+#print(get_files_info("calculator", "."))
+#print(get_files_info("calculator", "pkg"))
+print(get_files_info("calculator", "/bin"))
+#pprint(get_files_info("calculator", "../"))
---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+from functions.get_files_info import *
+
+
+class TestFunctions(unittest.TestCase):
+    
+    def test_path(self)
+        self.assertEqual_
+
+
+if __name__ == "__main__":
+    unittest.main()
